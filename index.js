9a1bb09d72821f90295bd92ace16b8d8aed92c0b
Anand Raman <codestryke@outlook.com>
2016-01-20 18:13:53 -0800

    fetching data from mongodb

--- index.js
+++ index.js
@@ -2,15 +2,33 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 var path = require('path');
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('mongodb://internaladmin:sarit@ds047355.mongolab.com:47355/portfoliotracking');
+var assert = require('assert');
 
-app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/'));
 
- app.get('/data', function(request, response) {
+// Make our db accessible to our router
+app.use(function(req,res,next){
+	req.db = db;
+	next();
+});
+
+ app.get('/filedata', function(request, response) {
    var readable = fs.createReadStream(path.join(__dirname, 'data/data.json'));
 	 readable.pipe(response);
  });
 
+app.get('/data', function(req, res) {
+	var db = req.db;
+	var collection = db.get('data');
+	collection.find({}, {}, function(e, docs) {
+		res.json(docs);
+	});
+});
+
+app.set('port', (process.env.PORT || 5000));
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 });
